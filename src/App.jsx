04503b7ae66c7a48c97c57a4d52b96ca0ef228bf
Debashish Boruah <debashishboruah54@gmail.com>
2025-03-15 01:54:12 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';import HelloKitty from './HelloKitty';
 import EnvelopeAnimation from './EnvelopeAnimation';
 import ImageWithBow from './ImageWithBow';
+import mainImage from './assets/47a7d3ca-d67d-4136-8b2c-18554d8209c8.JPG';
+import bowImage from './assets/cf19ab64ba4547db94fc43a28d9f7766-removebg-preview.png';
 ;
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
     <div className="App">
       <HelloKitty />
       <EnvelopeAnimation/>
-      <ImageWithBow mainImage={"/src/assets/47a7d3ca-d67d-4136-8b2c-18554d8209c8.JPG"} bowImage={"/src/assets/cf19ab64ba4547db94fc43a28d9f7766-removebg-preview.png"} caption={"When she wears Kurta ❤️"}/>
+      <ImageWithBow mainImage={mainImage} bowImage={bowImage} caption={"When she wears Kurta ❤️"}/>
     </div>
   );
 }
--- src/HelloKitty.jsx
+++ src/HelloKitty.jsx
@@ -2,6 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './HelloKittyDesign.css';
 import traditionalVideo from '/src/assets/traditional.mp4';
 import girlImage from './assets/IMG_6986.PNG';
+import kittyIcon from './assets/kitty.png';
+import heartIcon from './assets/e197de3a15ae6d541b8c331436dca89e-removebg-preview.png';
+import helloKittyIcon from './assets/17fa9cac4272d32b56270cf2f3bc9c70.jpg';
+import catImage from './assets/cat.jpg';
+import birthdayImage from './assets/6EC019F1-45F6-4659-9202-9078E3AEAA23.JPG';
+import kankanImage from './assets/kankan.png';
+import kitty6 from './assets/kitty6.jpg';
+import kitty7 from './assets/kitty7.jpg';
+import kitty12 from './assets/kitty12.jpg';
+import kitty2 from './assets/kitty2.jpg';
+import kitty9 from './assets/kitty9.jpg';
+import kitty11 from './assets/kitty11.jpg';
+import kitty13 from './assets/kitty13.jpg';
+import kitty5 from './assets/kitty5.jpg';
+import image1 from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.jpg';
+import image4 from './assets/image4.jpg';
+import image5 from './assets/image5.jpg';
+import image6 from './assets/image6.jpg';
+import image7 from './assets/image7.jpg';
+import image8 from './assets/image8.jpg';
+import image9 from './assets/image9.jpg';
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -20,17 +44,11 @@ const HelloKitty = () => {
   const [timeSince, setTimeSince] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const images = [
-    "/src/assets/image1.jpg",
-    "/src/assets/image2.jpg",
-    "/src/assets/image3.jpg",
-    "/src/assets/image4.jpg",
-    "/src/assets/image5.jpg",
-    "/src/assets/image6.jpg",
-    "/src/assets/image7.jpg",
-    "/src/assets/image8.jpg",
-    "/src/assets/image9.jpg"
-  ];
+  const imagesKitty = [
+    kitty6, kitty7, kitty12, kitty2, kitty9, kitty11, kitty13, kitty5
+];
+const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -38,6 +56,14 @@ const HelloKitty = () => {
     }, 3000); // Change image every 3 seconds
     return () => clearInterval(interval);
   }, [images.length]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setCurrentImageIndex(prevIndex => (prevIndex + 1) % imagesKitty.length);
+    }, 3000);
+    return () => clearInterval(interval);
+}, [imagesKitty.length]);
+
 
   // Calculate the time since you met
   useEffect(() => {
@@ -64,10 +90,6 @@ const HelloKitty = () => {
     };
   }, []);
 
-
-  // Placeholder for image - replace with actual image path
-  // const girlImage = "/src/assets/IMG_6986.PNG"; // Update this path
-
   return (
     <div className="hello-kitty-container">
       {/* Header Section */}
@@ -75,7 +97,7 @@ const HelloKitty = () => {
         <div className="title-container">
           <h1 className="title-text">To The</h1>
           <img 
-            src="/src/assets/17fa9cac4272d32b56270cf2f3bc9c70.jpg" // Update this path
+            src={helloKittyIcon} // Update this path
             alt="Hello Kitty" 
             className="hello-kitty-icon"
           />
@@ -96,14 +118,14 @@ const HelloKitty = () => {
         {/* Hello Kitty icons */}
         <div className="kitty-icon-bottom-left">
           <img 
-            src="/src/assets/kitty.png" // Update this path
+            src={kittyIcon} // Update this path
             alt="Hello Kitty icon" 
             className="kitty-small-icon"
           />
         </div>
         <div className="heart-icons-bottom-right">
           <img 
-            src="/src/assets/e197de3a15ae6d541b8c331436dca89e-removebg-preview.png" 
+            src={heartIcon}
             alt="Heart icon" 
             className="heart-icon"
           />
@@ -137,7 +159,7 @@ const HelloKitty = () => {
        {/* Header Section */}
        <div className="header-section">
         <img 
-          src="/src/assets/cat.jpg" 
+          src={catImage} 
           alt="Cute Cat" 
           className="cute-cat"
         />
@@ -147,7 +169,7 @@ const HelloKitty = () => {
       {/* Image Section */}
       <div className="image-container" data-aos="zoom-in-up">
         <img 
-          src="/src/assets/6EC019F1-45F6-4659-9202-9078E3AEAA23.JPG" 
+          src={birthdayImage}
           alt="21st Birthday" 
           className="birthday-image"
         />
@@ -178,7 +200,7 @@ const HelloKitty = () => {
       </div>
       
     <div className='kankan-image' >
-        <img src="/src/assets/kankan.png" alt="kankan" data-aos="zoom-out"/>
+        <img src={kankanImage} alt="kankan" data-aos="zoom-out"/>
     </div>
     <div className="date-section">
         <p className="date-label">Kankan ka Mommy</p>
@@ -194,15 +216,15 @@ const HelloKitty = () => {
        <div className="hello-kitty-section" >
         <h2 className="hello-kitty-title" data-aos="fade-up">She is a <span className="hello-kitty-highlight">HELLO KITTY</span> girl</h2>
         <div className="hello-kitty-images">
-          <img src="/src/assets/kitty6.jpg" alt="Hello Kitty 1" className="kitty-image" />
-          <img src="/src/assets/kitty7.jpg" alt="Hello Kitty 2" className="kitty-image" />
-          <img src="/src/assets/kitty12.jpg" alt="Hello Kitty 3" className="kitty-image" />
-          <img src="/src/assets/kitty2.jpg" alt="Hello Kitty Bow" className="kitty-image" />
+          <img src={kitty6} alt="Hello Kitty 1" className="kitty-image" />
+          <img src={kitty7} alt="Hello Kitty 2" className="kitty-image" />
+          <img src={kitty12} alt="Hello Kitty 3" className="kitty-image" />
+          <img src={kitty2} alt="Hello Kitty Bow" className="kitty-image" />
 
-          <img src="/src/assets/kitty9.jpg" alt="Hello Kitty 1" className="kitty-image" />
-          <img src="/src/assets/kitty11.jpg" alt="Hello Kitty 2" className="kitty-image" />
-          <img src="/src/assets/kitty13.jpg" alt="Hello Kitty 3" className="kitty-image" />
-          <img src="/src/assets/kitty5.jpg" alt="Hello Kitty Bow" className="kitty-image" />
+          <img src={kitty9} alt="Hello Kitty 1" className="kitty-image" />
+          <img src={kitty11} alt="Hello Kitty 2" className="kitty-image" />
+          <img src={kitty13} alt="Hello Kitty 3" className="kitty-image" />
+          <img src={kitty5} alt="Hello Kitty Bow" className="kitty-image" />
         </div>
       </div>
       
